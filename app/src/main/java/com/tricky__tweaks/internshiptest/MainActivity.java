73487d3fa74f5278c5f42ccc96331f4e3da8978e
Castell <pratikkatariya3.pk@gmail.com>
2020-03-18 13:41:40 -0500

    MainActivity - android on activity result implemented and music chooser is shown

--- app/src/main/java/com/tricky__tweaks/internshiptest/MainActivity.java
+++ app/src/main/java/com/tricky__tweaks/internshiptest/MainActivity.java
@@ -1,21 +1,29 @@
 package com.tricky__tweaks.internshiptest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_MUSIC_FILE = 1003;
     private TextView textViewFileName;
     private MaterialButton playBtn;
     private MaterialButton pauseBtn;
 
-    private void initViews(){
+    private Uri fileUri;
+
+    private void initViews() {
         textViewFileName = findViewById(R.id.activity_main_tv_file_name);
         playBtn = findViewById(R.id.activity_main_mb_play);
         pauseBtn = findViewById(R.id.activity_main_mb_pause);
@@ -28,9 +36,48 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
 
-        playBtn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "playing..", Toast.LENGTH_LONG).show());
+        playBtn.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "playing..", Toast.LENGTH_LONG).show();
+            showFileChooser();
+        });
 
         pauseBtn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "pause", Toast.LENGTH_SHORT).show());
 
+    }
+
+    public void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "select a File to Upload"), PICK_MUSIC_FILE);
+        } catch (android.content.ActivityNotFoundException xe) {
+            Toast.makeText(this, "please install a file manager", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == PICK_MUSIC_FILE && resultCode == RESULT_OK && data != null) {
+            fileUri = data.getData();
+            if (fileUri != null) {
+                Log.e("$$$  MAIN_ACTIVITY $$$", fileUri.getPath().toString());
+                Log.e("$$$  MAIN_ACTIVITY $$$ -- file name", queryName(fileUri));
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //This is code i found out on stack over flow to get the name of the file form uri
+    private String queryName(Uri uri) {
+        Cursor returnCursor = getContentResolver().query(uri, null, null, null, null);
+        assert returnCursor != null;
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        returnCursor.moveToFirst();
+        String name = returnCursor.getString(nameIndex);
+        returnCursor.close();
+        return name;
     }
 }
